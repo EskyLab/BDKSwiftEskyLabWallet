c81fdf7023958af86272925fd8c312ace78d19f3
Esky33 <djescaba@icloud.com>
2024-08-20 15:19:36 +0100

    check point ui design

---
+++ bdkFFI.framework/bdkFFI_framework.h
@@ -0,0 +1,19 @@
+//
+//  bdkFFI_framework.h
+//  bdkFFI.framework
+//
+//  Created by Esky Lab  on 20/08/2024.
+//  Copyright © 2024 CypherPunkCulture. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for bdkFFI_framework.
+FOUNDATION_EXPORT double bdkFFI_frameworkVersionNumber;
+
+//! Project version string for bdkFFI_framework.
+FOUNDATION_EXPORT const unsigned char bdkFFI_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bdkFFI_framework/PublicHeader.h>
+
+
---
+++ bdkFFI.frameworkTests/bdkFFI_frameworkTests.swift
@@ -0,0 +1,18 @@
+//
+//  bdkFFI_frameworkTests.swift
+//  bdkFFI.frameworkTests
+//
+//  Created by Esky Lab  on 20/08/2024.
+//  Copyright © 2024 CypherPunkCulture. All rights reserved.
+//
+
+import Testing
+@testable import bdkFFI_framework
+
+struct bdkFFI_frameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
