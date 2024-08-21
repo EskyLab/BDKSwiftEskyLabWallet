61b8fa3a62ceb6c68dd7725e036a61a3ac5fb496
Esky33 <djescaba@icloud.com>
2024-08-21 05:42:26 +0100

    Test complete working, upgraded most UI UX just a few tweaks left, then back to the stack

---
+++ Transaction.swift
@@ -0,0 +1,9 @@
+//
+//  Transaction.swift
+//  BDKSwiftExampleWallet
+//
+//  Created by Esky Lab  on 21/08/2024.
+//  Copyright © 2024 CypherPunkCulture. All rights reserved.
+//
+
+import Foundation
