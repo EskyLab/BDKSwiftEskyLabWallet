79092fffa781dbc9f7713b9ce92369ccedee82ff
Esky33 <djescaba@icloud.com>
2024-08-22 12:41:33 +0100

    pre-v1-CPC-1 splash screen and bio switch in settings tab

---
+++ BDKSwiftExampleWallet/View Model/SplashScreenView.swift
@@ -0,0 +1,19 @@
+//
+//  SplashScreenView.swift
+//  CypherPunkCulture
+//
+//  Created by Esky Lab  on 22/08/2024.
+//  Copyright © 2024 CypherPunkCulture. All rights reserved.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
