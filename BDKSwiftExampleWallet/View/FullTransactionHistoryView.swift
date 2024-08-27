f778abdc6fb99cd44b400134b2cbd7ab331c321e
EskyLab <info@cypherpunkculture.com>
2024-08-27 22:43:01 +0100

    updated transaction view.. to show last 10

---
+++ BDKSwiftExampleWallet/View/FullTransactionHistoryView.swift
@@ -0,0 +1,9 @@
+//
+//  FullTransactionHistoryView.swift
+//  CypherPunkCulture
+//
+//  Created by Esky Lab  on 27/08/2024.
+//  Copyright © 2024 CypherPunkCulture. All rights reserved.
+//
+
+import Foundation
